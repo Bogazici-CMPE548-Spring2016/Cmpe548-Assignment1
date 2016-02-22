c658fb8a2c0db73e77d700a482f887ef66041d30
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2016-02-22 02:05:43 +0200

    add problem2

--- problem2.m
+++ problem2.m
@@ -1,3 +1,16 @@
 
 % Problem 2
 
+p=0.7;
+u1 = 2*rand(10000,1)-1;
+u2 = 2*rand(10000,1)-1;
+sample = zeros(10000,2);
+
+for i=1:10000
+    if nthroot( abs(u1(i))^p + abs(u2(i))^p , p ) <= 1
+        sample(i,1) = u1(i);
+        sample(i,2) = u2(i);
+    end
+end
+
+plot(sample(:,1),sample(:,2),'.b');
